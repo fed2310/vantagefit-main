aac3f0d218552eb32187427abe4b824afc331482
Sagarika Dev Roy <sagarika.devroy@vantagecircle.com>
2020-09-10 14:30:16 +0530

    removed from gatsby-config.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://www.vantagefit.io`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://test.vantagecircle.in`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
